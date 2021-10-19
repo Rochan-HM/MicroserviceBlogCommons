b18e811204ec6512446064ebc9eabc2dbc1e9ee1
Rochan <rochana.hm@gmail.com>
2021-10-19 03:52:27 -0400

    Update Files

---
+++ src/messaging/pubsub/events/post-created-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from "../../subjects";
+
+export interface TicketCreatedEvent {
+    subject: Subjects.PostCreated;
+    data: {
+        id: string;
+        title: string;
+        body: number;
+        userId: string;
+    };
+}
